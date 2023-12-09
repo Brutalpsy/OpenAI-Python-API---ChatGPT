37a6b751f31e1cd5e65c0a6806cf60fda77934b2
psy <brutalpsy89@gmail.com>
2023-12-09 21:19:56 +0100

    added personality

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,25 +1,38 @@
 import openai
 from dotenv import dotenv_values
+import argparse
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-messages = []
-while True:
-    try:
-        user_input = input("You: ")
 
-        messages.append({"role": "user","content": user_input})
+def main():
+    parser = argparse.ArgumentParser(description="Simple command line chatbot with gpt-3.5")
+    parser.add_argument("--personality", type=str, help="A brief summary about chatbot personality", default= "Friendly and helpful chatbot")
 
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages)
-        
-        messages.append(response["choices"][0]["message"].to_dict())
-        print("Assistant: ", response["choices"][0]["message"]["content"])
+    args = parser.parse_args()
 
-    except KeyboardInterrupt:
-        print("Exiting...")
-        break
+    initial_prompt = f"You are a conversational chatbot. Your personality is: {args.personality}"
+    messages = [{"role": "system","content": initial_prompt}]
+    
+    while True:
+        try:
+            user_input = input("You: ")
 
-print(response)
+            messages.append({"role": "user","content": user_input})
+
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages)
+            
+            messages.append(response["choices"][0]["message"].to_dict())
+            print("Assistant: ", response["choices"][0]["message"]["content"])
+
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
