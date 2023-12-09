39453cc3a5f6522fa73048230c3ddf0433cee296
psy <brutalpsy89@gmail.com>
2023-12-09 21:01:13 +0100

    added persisting messages across requests

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -4,16 +4,19 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
+messages = []
 while True:
     try:
         user_input = input("You: ")
+
+        messages.append({"role": "user","content": user_input})
+
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{
-            "role": "user",
-            "content": user_input
-        }])
-        print(response["choices"][0]["message"]["content"])
+            messages=messages)
+        
+        messages.append(response["choices"][0]["message"].to_dict())
+        print("Assistant: ", response["choices"][0]["message"]["content"])
 
     except KeyboardInterrupt:
         print("Exiting...")
