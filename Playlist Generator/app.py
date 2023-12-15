80a5795982271ed4495d1268d9f2c4a4c2348501
psy <brutalpsy89@gmail.com>
2023-12-16 00:05:02 +0100

    created spotify playlist programmatically

--- Playlist Generator/app.py
+++ Playlist Generator/app.py
@@ -1,5 +1,6 @@
 import spotipy
 from dotenv import dotenv_values
+import pprint
 
 config = dotenv_values(".env")
 
@@ -7,8 +8,23 @@ sp = spotipy.Spotify(
     auth_manager = spotipy.SpotifyOAuth(
         client_id = config["SPOTIFY_CLIENT_ID"],
         client_secret = config["SPOTIFY_CLIENT_SECRET"],
-        redirect_uri = "http://localhost:9999"
+        redirect_uri = "http://localhost:9999",
+        scope="playlist-modify-private"
     )
 )
+current_user = sp.current_user()
 
-print(sp.current_user())
+assert current_user is not None
+
+search_results = sp.search(q="Uptown Funk", type="track", limit=10)
+
+tracks = [search_results["tracks"]["items"][0]["id"]]
+
+
+created_playlist = sp.user_playlist_create(
+    current_user["id"],
+    public = False,
+    name = "TESTING"
+)
+
+sp.user_playlist_add_tracks(current_user["id"],created_playlist["id"],tracks)
