3d3cd0262a3f1d33b2bcd40aa891d8910bde8bc3
psy <brutalpsy89@gmail.com>
2023-12-16 00:26:27 +0100

    adding in openai to playlist generator

--- Playlist Generator/app.py
+++ Playlist Generator/app.py
@@ -1,8 +1,45 @@
 import spotipy
 from dotenv import dotenv_values
-import pprint
+import json
+import openai
+
 
 config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
+
+def get_playlist(prompt, count=8):
+    example_json = """
+    [
+      {"song": "Everybody Hurts", "artist": "R.E.M."},
+      {"song": "Nothing Compares 2 U", "artist": "Sinead O'Connor"},
+      {"song": "Tears in Heaven", "artist": "Eric Clapton"},
+      {"song": "Hurt", "artist": "Johnny Cash"},
+      {"song": "Yesterday", "artist": "The Beatles"}
+    ]
+    """
+    messages = [
+        {"role": "system", "content": """You are a helpful playlist generating assistant. 
+        You should generate a list of songs and their artists according to a text prompt.
+        Your should return a JSON array, where each element follows this format: {"song": <song_title>, "artist": <artist_name>}
+        """
+        },
+        {"role": "user", "content": "Generate a playlist of 5 songs based on this prompt: super super sad songs"},
+        {"role": "assistant", "content": example_json},
+        {"role": "user", "content": f"Generate a playlist of {count} songs based on this prompt: {prompt}"},
+    ]
+
+    response = openai.ChatCompletion.create(
+        messages=messages,
+        model="gpt-4",
+        max_tokens=400
+    )
+
+    playlist = json.loads(response["choices"][0]["message"]["content"])
+    return playlist
+
+playlist = get_playlist("Synthwave songs",11)
+
+
 
 sp = spotipy.Spotify(
     auth_manager = spotipy.SpotifyOAuth(
@@ -15,10 +52,14 @@ sp = spotipy.Spotify(
 current_user = sp.current_user()
 
 assert current_user is not None
+track_ids = []
 
-search_results = sp.search(q="Uptown Funk", type="track", limit=10)
+for item in playlist:
+    artist, song = item["artist"], item["song"]
+    query = f"{song} {artist}"
+    search_results= sp.search(q=query, type="track", limit=10)
+    track_ids.append(search_results["tracks"]["items"][0]["id"])
 
-tracks = [search_results["tracks"]["items"][0]["id"]]
 
 
 created_playlist = sp.user_playlist_create(
@@ -27,4 +68,4 @@ created_playlist = sp.user_playlist_create(
     name = "TESTING"
 )
 
-sp.user_playlist_add_tracks(current_user["id"],created_playlist["id"],tracks)
+sp.user_playlist_add_tracks(current_user["id"],created_playlist["id"],track_ids)
