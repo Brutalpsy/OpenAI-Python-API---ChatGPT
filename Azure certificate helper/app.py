0a490f9ae82b2718c4b52ee366ad91633a2a9576
psy <brutalpsy89@gmail.com>
2023-12-16 18:52:39 +0100

    added first version of azure helper

---
+++ Azure certificate helper/app.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+
+import openai
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple command line utility")
+    parser.add_argument("-p", type=str, help="The prompt")
+
+    args = parser.parse_args()
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    prompt = args.p
+    answer = get_answer(prompt)
+    print(answer)
+
+def get_answer(prompt):
+    messages = [
+        {"role": "system", "content": """You are an expert Azure Developer with a broad knowlage of the azure platform 
+        and azure ecosystem, i will ask you some questions that i have for the azure 204 exam, you will provide me with answer(s) and explanation on those questions. 
+        Response should be in format: 
+         Answer: 
+		 <assistant_answer>
+		 Explanation:
+         <assistant_explanation> 
+        """   
+        },
+        {"role": "user", "content": f"{prompt}"},
+    ]
+
+    response = openai.ChatCompletion.create(
+        messages=messages,
+        model="gpt-4"
+    )
+
+    answer = response["choices"][0]["message"]["content"]
+    return answer
+
+if __name__ == "__main__":
+    main()
